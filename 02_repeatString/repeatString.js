9400f62d77b12b202996b2ca03c37247480ea5ea
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-08 21:44:35 -0600

    02_repeatString complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+  if (n < 0) return "ERROR";
 
+  let repeatedStr = "";
+
+  while(n) {
+    repeatedStr += str;
+
+    n--;
+  }
+
+  return repeatedStr;
 };
 
 // Do not edit below this line
