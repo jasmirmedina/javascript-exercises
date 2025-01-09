6b48254be566789ab2f3197cf9da1bddfa7354cc
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-08 22:56:55 -0600

    05_sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if(
+    start < 0 || end < 0 ||
+    !Number.isInteger(start) || !Number.isInteger(end)
+  ) return "ERROR";
 
+  const min = Math.min(...[start, end]);
+  const max = Math.max(...[start, end]);
+
+  let num = 0;
+
+  for(let i = min; i <= max; i++) {
+    num += i;
+  }
+
+  return num;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
