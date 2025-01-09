51e94048ff39dd3ddb6d9afbe9e1a64c745486d5
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-09 13:23:54 -0600

    06_leapYears complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if(year % 400 === 0) {
+    return true;
+  } else if(year % 100 === 0) {
+    return false;
+  } else if(year % 4 === 0) {
+    return true;
+  }
 
+  return false;
 };
+
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
