5714a3df3cfa58ccff52b425013abdef06469bce
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-11 20:03:50 -0600

    10_fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  const n1 = parseInt(n);
 
+  if(isNaN(n1)) return "OOPS";
+  if(n1 < 0) return "OOPS";
+
+  return n1 <= 1 ? n1 : fibonacci(n1 - 1) + fibonacci(n1 - 2);
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
