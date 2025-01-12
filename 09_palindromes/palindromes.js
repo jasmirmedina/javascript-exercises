90756535fbbcfbe27a8c97d1941f7eac24bc196a
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-11 19:45:21 -0600

    09_palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((i) => alpha.includes(i))
+    .join("");
+
+  const reversedStr = cleanStr.split("").reverse().join("");
+
+  return cleanStr === reversedStr;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
