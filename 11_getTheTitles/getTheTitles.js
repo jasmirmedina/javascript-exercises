c5887105dce3d4efc1c5eb8e65c02c5af6c11e76
jasmirmedina <jasmirmedina56@gmail.com>
2025-01-11 21:01:14 -0600

    11_getTheTitles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  return books.map((i) => i.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
